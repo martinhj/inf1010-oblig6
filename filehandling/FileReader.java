d7e29dbf617aad492fb3c04111688126214a2eeb
martinhj <martinhj@gmail.com>
2013-05-08 09:40:57 +0200

    Added filereader - now it prints the file back at the terminal

--- filehandling/FileReader.java
+++ filehandling/FileReader.java
@@ -1,8 +1,33 @@
 package filehandling;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 /**
- *
+ * This class reads a file with words. The first line is the numbers of lines.
  */
-class filehandling {
-    public filehandling() {
+class FileReader {
+    public FileReader(String filename) throws FileNotFoundException {
+    	readFile(filename);
+    }
+    /**
+     *
+     */
+    private Scanner openFile(String filename) throws FileNotFoundException {
+    	File file = new File(filename);
+    	Scanner sc = new Scanner(file);
+    	return sc;
+    }
+    /**
+     * 
+     */
+    private void readFile(String filename) 
+    	throws FileNotFoundException, InputMismatchException {
+    	Scanner sc = openFile(filename);
+    		int numberOfLines = (sc.nextInt());
+    	System.out.println(numberOfLines);
+    	/*while (sc.hasNextLine()) {
+    		System.out.println(sc.nextLine());
+    	}*/
     }
 }
