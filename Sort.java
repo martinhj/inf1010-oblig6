2e0f0c5de4e744b43b546ce7c575f395a51a846c
martinhj <martinhj@gmail.com>
2013-05-08 11:27:55 +0200

    Added use of the FileReader in the Sort class with exception handling

--- Sort.java
+++ Sort.java
@@ -1,12 +1,30 @@
+import filehandling.*;
+import logic.*;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 /**
- * A class for sorting strngs.
+ * A class for sorting strings.
  * Will use ... sort algorithm.
  */
 class Sort {
     public Sort(int numberOfThreads, String fileToSort, String outputFile) {
-       System.out.println(numberOfThreads + " " + fileToSort + " " + outputFile); 
+    	String [] strings = new String [0];
+		try {
+			strings = new FileReader(fileToSort).getStrings();
+		} catch (FileNotFoundException e) {
+			System.out.println("E. flntfnd. ");
+			System.exit(1);
+		} catch (InputMismatchException e) {
+			System.out.println("E. inputmism. ");
+			System.exit(2);
+		} catch (NoSuchElementException e) {
+			System.out.println("E. nschlmnt. ");
+			System.exit(3);
+		}
+
     }
     public static void main (String [] args) {
-        new Sort(Integer.getInteger(args[0]), args[1], args[2]);
+        new Sort(Integer.parseInt(args[0]), args[1], args[2]);
     }
 }
