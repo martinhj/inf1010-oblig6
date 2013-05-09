8e13328f38f6bd3bf4bcd1d779eb29bbfce9d15e
martinhj <martinhj@gmail.com>
2013-05-09 17:54:44 +0200

    Changed the code to correspond with the assignment text

--- logic/Sorter.java
+++ logic/Sorter.java
@@ -1,4 +1,6 @@
 package logic;
+
+import java.util.Arrays;
 /**
  * This class does the sorting.
  */
@@ -12,14 +14,29 @@ public class Sorter {
 	public Sorter(int numberOfThreads, String [] strings) {
 		this.numberOfThreads = numberOfThreads;
 		this.array = strings;
+
 		// denne trengs å endre til å bruke numberOfThreads og forholde seg til
 		// stringene::
 		arraysOfArrays = fillArrays(arraysOfArrays, readyArrays);
-		// 1. dele opp arrayet i like mange array som tråder
-		//		sette opp disse i et array med pekere til disse
-		//		sette opp et bool-array med true / false
-		//
+		/*for (String [] a: arraysOfArrays)
+			Arrays.sort(a);*/
+		int numbersOfWords = 0;
+		for (String [] a: arraysOfArrays) {
+			System.out.println("Her: ");
+			for (String s: a) {
+				System.out.println(s);
+				numbersOfWords++;
+			}
+		}
+		System.out.println(":" + array.length);
+		System.out.println(numbersOfWords);
+		//ok 1. dele opp arrayet i like mange array som tråder
+		//ok		sette opp disse i et array med pekere til disse
+		//ok		sette opp et bool-array med true / false
+		// SÅ: hver tråd må ha peil på hvilket array den forholder seg til 
+		// (array index - denne skal brukes opp mot string-array og bool-array).
 		// 2. starte opp sortering i hver tråd.
+		
 		// 3. når en tråd er ferdig sjekker den om det er noen andre ferdige, sjekk
 		// bool-array..
 		// 4. hvis ingen andre ferdige wait.
@@ -28,6 +45,8 @@ public class Sorter {
 		//		bool-array med hvilken som er ferdig.
 		// 7. hvis ingen andre ferdige, vent
 		// 8. 
+		System.out.println(array.length);
+		System.out.println(array.length % numberOfThreads);
 		System.out.println(numberOfArrays);
 	}
 	/**
@@ -42,23 +61,49 @@ public class Sorter {
 			b = false;
 		allArrays = new String[numberOfThreads][];
 		// skal dette gjøres med tråder?
-		for (int i = 0; i < array.length; i+=arrayLength()) {
+	/*	for (int i = 0; i < array.length; i+=arrayLength()) {
 			if (beforeLastArray(i)) 
 				allArrays[whichArray(i)] = fillArray(array, arrayLength(), i);
 			if (lastArray(i)) 
 				allArrays[whichArray(i)] = fillArray(array, lastArrayLength(), i);
+		}*/
+
+		for (int i = 0; i < array.length; i+=arrayLength()) {
+			if (beforeLastArray(i)) {
+				System.out.println("Legger inn " + whichArray(i));
+				allArrays[whichArray(i)] = fillArray(array, arrayLength(), i);
+			}
 		}
+		for (int i = 0; i < rest(); i++) {
+			System.out.println("Legger inn " + i);
+			System.out.println(allArrays[i].length);
+			allArrays[i][allArrays[i].length - 1] = array[array.length - rest()];
+		}
+		
 		return allArrays;
 	}
+
+	// 1. dele array i antallTråder deler
+	// 2. rest legges på de første rest antall tråder
+	// 3. de første rest antall trådene skal være lengde på 
+	// antallord / antalltråder + 1
+	// 4. alle tråder fylles
+	// 5. rest fyller et ord inn på siste plass i de første rest antall tråder
+
 	private String [] fillArray(String [] oldArray, int length, int start) {
-		String [] newArray = new String [length];
+		numberOfArrays++;
+		String [] newArray = new String [0];
+		if (numberOfArrays <= rest())
+			newArray = new String [length + 1];
+		if (numberOfArrays > rest())
+			newArray = new String [length];
 		for (int i = 0; i < length; i++)
 			newArray[i] = oldArray[start + i];
-		numberOfArrays++;
+		System.out.println(newArray.length);
 		return newArray;
 	}
 	private boolean beforeLastArray(int i) {
-		if (i / arrayLength() < numberOfThreads - 1) return true;
+		if (i / arrayLength() < numberOfThreads) return true;
 		return false;
 	}
 	private boolean lastArray(int i) {
@@ -67,6 +112,9 @@ public class Sorter {
 	}
 	private int lastArrayLength() {
 		return arrayLength() + array.length % numberOfThreads;
+	}
+	private int rest() {
+		return array.length % numberOfThreads;
 	}
 	private int arrayLength() {
 		return array.length / numberOfThreads;
