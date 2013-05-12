0cffab3409d80ebdf201cee9e0dbc66d9e60bc1b
martinhj <martinhj@gmail.com>
2013-05-12 13:42:26 +0200

    Added many lines to test different count of threads

--- run.sh
+++ run.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-java Sort 4 testfiles/shortNames.txt out.txt
+java Sort 1 testfiles/names.txt out.txt
+java Sort 1 testfiles/names.txt out.txt
+java Sort 1 testfiles/names.txt out.txt
+java Sort 2 testfiles/names.txt out.txt
+java Sort 2 testfiles/names.txt out.txt
+java Sort 2 testfiles/names.txt out.txt
+java Sort 3 testfiles/names.txt out.txt
+java Sort 3 testfiles/names.txt out.txt
+java Sort 3 testfiles/names.txt out.txt
+java Sort 4 testfiles/names.txt out.txt
+java Sort 4 testfiles/names.txt out.txt
+java Sort 4 testfiles/names.txt out.txt
+java Sort 16 testfiles/names.txt out.txt
+java Sort 16 testfiles/names.txt out.txt
+java Sort 16 testfiles/names.txt out.txt
+java Sort 32 testfiles/names.txt out.txt
+java Sort 32 testfiles/names.txt out.txt
+java Sort 32 testfiles/names.txt out.txt
+java Sort 128 testfiles/names.txt out.txt
+java Sort 128 testfiles/names.txt out.txt
+java Sort 128 testfiles/names.txt out.txt
