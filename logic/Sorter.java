8457f7e23a159f3aef4d5c3b33c01e715b40abc6
martinhj <martinhj@gmail.com>
2013-05-12 09:26:11 +0200

    Now the sorting is done recursively

--- logic/Sorter.java
+++ logic/Sorter.java
@@ -1,77 +1,23 @@
 package logic;
 
-import java.util.HashMap;
 /**
  * This class does the sorting.
  */
 public class Sorter {
-	private String [] allWordsUnsorted;
-	private int numbersOfWords;
+	private int numberOfWords;
 	private int numberOfThreads;
-	private String [][] arraysOfArrays;
-	private boolean [] readyArrays;
-	private int numberOfArrays = 0;
-    private SSLLcontainer<String,String> sortedWords =
-        new SSLLcontainer<String,String>();
-	private String [] allWordsSorted;
+	private String [] allWordsUnsorted;
 	public Sorter(int numberOfThreads, String [] strings) {
 		this.numberOfThreads = numberOfThreads;
 		this.allWordsUnsorted = strings;
-		this.numbersOfWords = allWordsUnsorted.length;
-		arraysOfArrays = fillArrays(arraysOfArrays, readyArrays);
-		
-		// Splitt opp i forskjellige tråder her.
-		sortArrays(arraysOfArrays);
-		for (int i = 0; i < arraysOfArrays.length; i++) {
-			arraysOfArrays[i] = sortArray(arraysOfArrays[i]);
-		}
-		
-		
-		
-		
-		allWordsSorted = new String [numbersOfWords];
-		int wordIndex = 0;
-		for (int i = 0; i < arraysOfArrays.length; i++)
-			for (int j = 0; j < arraysOfArrays[i].length; j++)
-                sortedWords.insert(arraysOfArrays[i][j], arraysOfArrays[i][j]);
-	//	for (String s : allWordsSorted)
-	//			System.out.println(s);
+		this.numberOfWords = allWordsUnsorted.length;
+		for (String s : allWordsUnsorted) System.out.println(s);
+		System.out.println("-------");
+		allWordsUnsorted = splitArray(allWordsUnsorted);
+		for (String s: allWordsUnsorted) System.out.println(s);
 
-		// debug printout:::
-		/*for (String [] a: arraysOfArrays)
-		int numbersOfWords = 0;
-		for (String [] a: arraysOfArrays) {
-			System.out.println("Her: ");
-			for (String s: a) {
-				System.out.println(s);
-				numbersOfWords++;
-			}
-		}*/
-		/*System.out.println(":" + array.length);*/
-		//System.out.println(numbersOfWords);
-		//ok 1. dele opp arrayet i like mange array som tråder
-		//ok		sette opp disse i et array med pekere til disse
-		//ok		sette opp et bool-array med true / false
-		// SÅ: hver tråd må ha peil på hvilket array den forholder seg til 
-		// (array index - denne skal brukes opp mot string-array og bool-array).
-		// 2. starte opp sortering i hver tråd.
 		
-		// 3. når en tråd er ferdig sjekker den om det er noen andre ferdige, sjekk
-		// bool-array..
-		// 4. hvis ingen andre ferdige wait.
-		// 5. hvis andre ferdige notify.
-		// 6. hvis melding fra en annen om at det er på tide å våkne, sjekke
-		//		bool-array med hvilken som er ferdig.
-		// 7. hvis ingen andre ferdige, vent
-		// 8. 
-	}
 
-	private void insertInSortedArray(String [] insertArray, String wordToInsert)
-	{
-		int i = 0;
-		while (insertArray[i++] != null) {
-			//if (wordToInsert.compareTo(insertArray[i]) > 0)
-		}
 	}
 	/**
 	 * @return the sorted array.
@@ -79,19 +25,20 @@ public class Sorter {
 	public String [] getArray() {
 		return allWordsUnsorted;
 	}
-	private String[][] fillArrays(String [][] allArrays, boolean [] readyArrays) {
+	private String[][] fillArrays(String [][] allArrays, boolean [] readyArrays)
+	{
 		readyArrays = new boolean[numberOfThreads];
 		for (boolean b : readyArrays)
 			b = false;
 		allArrays = new String[numberOfThreads][];
-		for (int i = 0; i < numbersOfWords; i+=arrayLength()) {
+		for (int i = 0; i < numberOfWords; i+=arrayLength()) {
 			if (beforeRest(i))
 				allArrays[whichArray(i)] =
 					fillArray(allWordsUnsorted, arrayLength(), i);
 		}
 		for (int i = 0; i < rest(); i++) {
 			allArrays[i][allArrays[i].length - 1] = 
-				allWordsUnsorted[numbersOfWords - rest()];
+				allWordsUnsorted[numberOfWords - rest()];
 		}
 		return allArrays;
 	}
@@ -104,14 +51,9 @@ public class Sorter {
 	}
 
 	private String [] fillArray(String [] oldArray, int length, int start) {
-		numberOfArrays++;
-		String [] newArray = new String [0];
-		if (numberOfArrays <= rest())
-			newArray = new String [length + 1];
-		if (numberOfArrays > rest())
-			newArray = new String [length];
-		for (int i = 0; i < length; i++)
-			newArray[i] = oldArray[start + i];
+		String [] newArray = new String [length];
+			for (int i = 0; i < length; i++)
+				newArray[i] = oldArray[start + i];
 		return newArray;
 	}
 	private boolean beforeRest(int i) {
@@ -123,15 +65,63 @@ public class Sorter {
 		return false;
 	}
 	private int lastArrayLength() {
-		return arrayLength() + numbersOfWords % numberOfThreads;
+		return arrayLength() + numberOfWords % numberOfThreads;
 	}
 	private int rest() {
-		return numbersOfWords % numberOfThreads;
+		return numberOfWords % numberOfThreads;
 	}
 	private int arrayLength() {
-		return numbersOfWords / numberOfThreads;
+		return numberOfWords / numberOfThreads;
+	}
+	private int minArrayLength() {
+		return arrayLength() + 1;
 	}
 	private int whichArray(int i) {
 		return i / arrayLength();
+	}
+	/**
+	 * Splits the array in two recursively until it reaches the minimum length
+	 * of the arrays.
+	 */
+	public String [] splitArray(String [] words) {
+		if (words.length > minArrayLength()) {
+			String [] firstArray;
+			String [] lastArray;
+			firstArray = fillArray(words, words.length / 2, 0);
+			for (String s: firstArray) System.out.println(s);
+			System.out.println(":::");
+			firstArray = splitArray(firstArray);
+			lastArray = fillArray(words, words.length / 2 + words.length % 2, words.length / 2);
+			for (String s: lastArray) System.out.println(s);
+			System.out.println("::");
+			lastArray = splitArray(lastArray);
+			words = mergeArrays(firstArray, lastArray);
+		}
+		if (words.length <= minArrayLength()) {
+			words = sortArray(words);
+		}
+		return words;
+	}
+	/**
+	 * Merge two sorted arrays into one sorted array.
+	 * @return a sorted array.
+	 */
+	private String [] mergeArrays(String [] firstArray, String [] lastArray) {
+		String [] sorted = new String [firstArray.length + lastArray.length];
+		int i = 0, j = 0, k = 0;
+		while (j < firstArray.length && k < lastArray.length) {
+			if (firstArray[j].compareTo(lastArray[k]) < 0)
+				sorted[i] = firstArray[j++];
+			else 
+				sorted[i] = lastArray[k++];
+			i++;
+		}
+		while (j < firstArray.length) {
+			sorted[i++] = firstArray[j++];
+		}
+		while (k < lastArray.length) {
+			sorted[i++] = lastArray[k++];
+		}
+		return sorted;
 	}
 }
